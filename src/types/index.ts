1be5ea05d5ea238fffae8bf1ca0e5e67ec0c214b
Anastasia Kulesh <kulesh.anastasia.s@gmail.com>
2024-03-25 02:19:56 +0300

    refactor: change type arrangement

---
+++ src/types/index.ts
@@ -0,0 +1,31 @@
+interface ICard {
+    category: string;
+    name: string;
+    description: string;
+    price: number;
+    image: string;
+}
+
+interface ICardsList {
+    cards: ICard[];
+}
+
+interface ICustomer {
+    paymentType: 'online' | 'offline';
+    address: string;
+    email: string;
+    phone: string;
+    setPaymentType(type: string): void;
+    setAddress(address: string): void;
+    setEmail(email: string): void;
+    setPhone(phone: string): void;
+}
+
+interface IBasket {
+    items: ICard[];
+    get сountItems(): void;
+    get costItems(): void;
+    addItem(item: ICard): void;
+    deleteItem(item: ICard): void;
+    makeOrder(): boolean;
+}
